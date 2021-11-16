55f9e88bdc28ef1876caaead61778b49d5e98c96
Aki Salmi <aki.salmi@iki.fi>
2021-11-16 06:34:16 +0200

    :white_check_mark: Add sketch of outside-in tests.
    Theese will be omitted, and let's do more inside-out TDD with the cycle of 'remove duplication' and 'write better names'

---
+++ test/domain/can-print-total.spec.ts
@@ -0,0 +1,10 @@
+describe('Point-of-Sale keeps track of total', () => {
+
+
+    describe('summing total', () => {
+
+    })
+    // 0, 1, many, ∞, 💥
+    it('should be done here, but let\'s wait for a while')
+
+})
--- test/mocha/acceptance.spec.ts
+++ test/mocha/acceptance.spec.ts
@@ -1,8 +1,8 @@
-import chai, {expect} from 'chai'
+import chai, { expect } from 'chai'
 import sinonChai from 'sinon-chai'
-import {LCDDisplay} from '../../src/domain/output/LCDDisplay'
-import {AddItem, AddItemWithBarcode} from '../../src/domain/actions/addItem'
-import {NoItemFound, Stock} from '../../src/domain/repository/stock'
+import { LCDDisplay } from '../../src/domain/output/LCDDisplay'
+import { AddItem, AddItemWithBarcode } from '../../src/domain/actions/addItem'
+import { NoItemFound, Stock } from '../../src/domain/repository/stock'
 
 chai.use(sinonChai)
 const notCalledEver = '-1.999€'
@@ -24,7 +24,7 @@ const fakeStockWith: (barcodesByPrice: Record<string, string>) => Stock =
     (barcodesByPrice) => ({
         findItem: (barcode: string) => {
             if (barcode in barcodesByPrice) {
-                return Promise.resolve({asString: () => barcodesByPrice[barcode]})
+                return Promise.resolve({ asString: () => barcodesByPrice[barcode] })
             }
             return Promise.resolve(new NoItemFound(barcode))
         },
@@ -33,7 +33,7 @@ const fakeStockWith: (barcodesByPrice: Record<string, string>) => Stock =
 
 describe('Point of Sale system', () => {
     it('should print the price in the LCD', async () => {
-        const stock = fakeStockWith({12345: '10,50€'})
+        const stock = fakeStockWith({ 12345: '10,50€' })
         const lcdDisplay: TestSpecificLCDDisplay = new TestSpecificLCDDisplay()
         const onReadBarcode: AddItem = new AddItemWithBarcode(lcdDisplay, stock)
 
@@ -44,7 +44,7 @@ describe('Point of Sale system', () => {
     })
 
     it('should not print the price of an iteM that is not found', async () => {
-        const stock = fakeStockWith({12345: '10.45'})
+        const stock = fakeStockWith({ 12345: '10.45' })
         const lcdDisplay = new TestSpecificLCDDisplay()
         const onReadBarcode = new AddItemWithBarcode(lcdDisplay, stock)
 
@@ -52,6 +52,37 @@ describe('Point of Sale system', () => {
 
         const expected = lcdDisplay.lastCall()
         expect(expected).to.eql('Product not found: 54321')
+    })
+
+    describe('can sell multiple items', () => {
+
+        it.skip('should print the total and individual prices', async () => {
+            const stock = fakeStockWith(
+                {
+                    12345: '10.45',
+                    23456: '1.40',
+                    34567: '81.10',
+                },
+            )
+            const lcdDisplay = new TestSpecificLCDDisplay()
+            const onReadBarcode = new AddItemWithBarcode(lcdDisplay, stock)
+
+            await onReadBarcode.onReadBarcode('12345')
+            await onReadBarcode.onReadBarcode('23456')
+            await onReadBarcode.onReadBarcode('34567')
+
+            // const pointOfSale
+            // const expected = await pointOfSale.total()
+
+            // expect(expected).to.eql('Product not found: 54321')
+        })
+
+
+        // 0, 1, many, ∞, 💥
+        it('should print total even if one item is not found')
+        it('can be possible to override price of an item')
+        it('pressing total when no items read, should not do a thing')
+        it( 'when empty barcode!')
     })
 })
 
